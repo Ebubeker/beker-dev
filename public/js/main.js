41f2d0b71311ae928670921c36c3a674f2bf9d53
Ebubeker <ebubeker1rexha@gmail.com>
2021-10-09 15:43:38 +0200

    loader function

--- public/js/main.js
+++ public/js/main.js
@@ -65,9 +65,37 @@ window.addEventListener('resize', function(event) {
 
 //onload effect
 const loader = document.querySelector('.loader');
+const loaderBelow = document.querySelector('.loaderBelow');
 
 window.onload = () => {
     setTimeout(() => {
-        loader.style.display = 'none';
+        const loader_one = document.querySelector('.loader-one-up'),
+            loader_two = document.querySelector('.loader-two-up'),
+            loader_three = document.querySelector('.loader-one-down'),
+            loader_four = document.querySelector('.loader-two-down');
+
+        console.log(loader_three)
+
+        loader_one.classList.add('loader-1');
+        loader_two.classList.add('loader-3');
+        setTimeout(() => {
+            loader_three.classList.add('loader-2');
+            loader_four.classList.add('loader-4');
+            setTimeout(() => {
+                loader.style.display = 'none';
+            }, 1200)
+            setTimeout(() => {
+                loaderBelow.style.display = 'none';
+                let h1 = document.getElementById('second-writing');
+                let h1_one = document.getElementById('first-writing');
+                h1_one.classList.add('typing-demo-one');
+                setTimeout(() => {
+                    document.querySelector('.typing-demo-one').style.border = 'none';
+                    h1.classList.add('typing-demo-two');
+                    document.querySelector('.typing-demo-two').style.border = 'none';
+                }, 1000);
+            }, 1500);
+        }, 300);
+
     }, 1500);
 }
--- public/js/writing.js
+++ public/js/writing.js
@@ -1,8 +1,8 @@
-let h1 = document.getElementById('second-writing');
-setTimeout(() => {
-    document.querySelector('.typing-demo-one').style.border = 'none';
-    h1.classList.add('typing-demo-two');
-    setTimeout(() => {
-        document.querySelector('.typing-demo-two').style.border = 'none';
-    }, 2000);
-}, 2000);
+// let h1 = document.getElementById('second-writing');
+// setTimeout(() => {
+//     document.querySelector('.typing-demo-one').style.border = 'none';
+//     h1.classList.add('typing-demo-two');
+//     setTimeout(() => {
+//         document.querySelector('.typing-demo-two').style.border = 'none';
+//     }, 2000);
+// }, 2000);
